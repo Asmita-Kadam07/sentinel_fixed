768d958f781a17193a2562d8105471fd6f859ebc
Asmita-Kadam07 <kadamasmita710@gmail.com>
2026-05-14 13:02:21 +0530

    Update next.config.mjs

--- apps/dashboard/next.config.mjs
+++ apps/dashboard/next.config.mjs
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ['@sentinel/shared'],
+
   experimental: {
     serverComponentsExternalPackages: ['@libsql/client'],
+  },
+
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
